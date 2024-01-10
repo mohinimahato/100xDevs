100e05c327621453362488e8f4ab91d478a972a5
Mohini Mahato <mohinimahatowork@gmail.com>
2024-01-11 01:05:07 +0530

    Hard: Class based assignment(Implement todo.js)

---
+++ week-1/Homework/01-js/hard/todo.js
@@ -0,0 +1,67 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+  Once you've implemented the logic, test your code by running
+*/
+
+class Todo {
+    constructor() {
+        this.todos = []
+    }
+    add(todo) {
+        this.todos.push(todo)
+    }
+    remove(indexOfTodo) {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+            this.todos.splice(indexOfTodo, 1);
+        } else {
+            console.log('Invalid index')
+        }
+    }
+    update(index, updatedTodo) {
+        if (index >= 0 && index < this.todos.length) {
+            this.todos[index] = updatedTodo;
+        } else {
+            console.log("Invalid Index")
+        }
+    }
+    getAll() {
+        return this.todos
+    }
+    get(indexOfTodo) {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+            return this.todos[indexOfTodo];
+        } else {
+            console.log('Invalid index');
+            return null
+        }
+    }
+    clear() {
+        this.todos = []
+    }
+}
+
+const todoList = new Todo();
+
+todoList.add("Learn Javascript");
+todoList.add("Buy groceries");
+todoList.add("Finish homework");
+todoList.add("Go for a run");
+
+console.log("Todos: ", todoList.getAll());
+console.log("_______________________")
+todoList.update(1, "Finish coding assignment")
+console.log('Update todo at index 1: ', todoList.get(1))
+console.log("_______________________")
+todoList.remove(0);
+console.log("Remaining Todos: ", todoList.getAll())
+console.log("_______________________")
+todoList.clear();
+console.log("Cleared Todos:", todoList.getAll())
+// module.exports = Todo;
