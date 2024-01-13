b8ecfd058b6c4c1c3fd069fd9e8978e3cc33f535
Mohini Mahato <mohinimahatowork@gmail.com>
2024-01-14 01:30:12 +0530

    useMemo

--- OngoingClassProjects/Week6/Project002_07_01_2024/src/App.jsx
+++ OngoingClassProjects/Week6/Project002_07_01_2024/src/App.jsx
@@ -1,23 +1,33 @@
-import { useEffect } from 'react'
+import { useMemo } from 'react';
 import { useState } from 'react'
 // import axios from 'axios';
 
+//useMemo
 function App() {
+  const [inputValue, setinputValue] = useState(1);
+  const [counter, setCounter] = useState(0)
+
+  let count = useMemo(() => {
+    let sum = 0;
+    for (let i = 1; i < inputValue; i++) {
+      sum = sum + i
+    }
+    return sum
+  }, [inputValue])
 
   return <div>
+    <dir>
+      <input onChange={function (e) {
+        setinputValue(e.target.value)
+      }} placeholder="Enter number" />
+      <p>Sum from 1 to {inputValue} is {count}</p>
+      <button onClick={() => {
+        setCounter(counter + 1)
+      }}>Sum of 0 to ({counter})</button>
+    </dir>
 
-    <Todo id={btnId} />
   </div>
 
-}
-
-function Todo({ id }) {
-
-  return <div>
-    Id:{id}
-    <h1>{todo.title}</h1>
-    <h5>{todo.description}</h5>
-  </div>
 }
 
 export default App
