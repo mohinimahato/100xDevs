f95e2876dfa003e459239715f6ee799015570fd1
Mohini Mahato <mohinimahatowork@gmail.com>
2024-03-04 00:23:34 +0530

    Understood the basic difference between SSR and CSR

---
+++ week-14/my-app/components/SigninBtn.tsx
@@ -0,0 +1,16 @@
+'use client';
+export function SigninBtn(){
+    function handler(){
+        console.log('hi')
+    }
+    return <div>
+         <div className="pt-2">
+                        <button onClick={handler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                    </div>
+    </div>
+}
+interface LabelledInputType {
+    label: string;
+    placeholder: string;
+    type?: string;
+}
--- week-14/my-app/components/SigninCompenent.tsx
+++ week-14/my-app/components/SigninCompenent.tsx
@@ -1,3 +1,4 @@
+import { SigninBtn } from "./SigninBtn";
 export function SigninComponent(){
     function handler(){
         console.log('hi')
@@ -14,7 +15,10 @@ export function SigninComponent(){
                     <div className="pt-2">
                         <LabelledInput label="Username" placeholder="mohini@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button onClick={handler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        
+                        <div>
+                            <SigninBtn/>
+                        </div>
                     </div>
                 </div>
             </a>
