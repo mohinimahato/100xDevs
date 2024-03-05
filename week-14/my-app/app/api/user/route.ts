b6f29d4279eb458091e8d86513c6b98e843cafaf
Mohini Mahato <mohinimahatowork@gmail.com>
2024-03-06 01:23:29 +0530

    Backend Signin

--- week-14/my-app/app/api/user/route.ts
+++ week-14/my-app/app/api/user/route.ts
@@ -1,10 +1,10 @@
-export function GET(){
-    // database logic
-    return  Response.json({
-        email: "mohini@gmail.com",
-        name:"mohini"
-    })
-}
+// export function GET(){
+//     // database logic
+//     return  Response.json({
+//         email: "mohini@gmail.com",
+//         name:"mohini"
+//     })
+// }
 // In express we write it like
 /**
 app.get("/api/user". (req,res)=>{
@@ -16,3 +16,16 @@ app.get("/api/user". (req,res)=>{
 
  * 
  */
+
+import { NextRequest } from "next/server";
+export async function POST(req:NextRequest) {
+    // extract the body
+    const body = await req.json();
+    // store the body in the database
+    console.log(body);
+
+    return Response.json({
+        message: "You are logged in!"
+    })
+    
+}
--- week-14/my-app/components/SigninCompenent.tsx
+++ week-14/my-app/components/SigninCompenent.tsx
@@ -1,8 +1,13 @@
-import { SigninBtn } from "./SigninBtn";
+"use client"
+import axios from "axios";
+// import { SigninBtn } from "./SigninBtn";
+
+import { ChangeEventHandler, useState } from "react";
+
 export function SigninComponent(){
-    function handler(){
-        console.log('hi')
-    }
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   
     return <div  className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -13,12 +18,23 @@ export function SigninComponent(){
                         </div>
                     </div>
                     <div className="pt-2">
-                        <LabelledInput label="Username" placeholder="mohini@gmail.com" />
-                        <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        
-                        <div>
-                            <SigninBtn/>
-                        </div>
+                    <LabelledInput onChange={(e) => {
+                            setUsername(e.target.value);
+                        }} label="Username" placeholder="harkirat@gmail.com" />
+                        <LabelledInput onChange={(e) => {
+                            setPassword(e.target.value)
+                        }} label="Password" type={"password"} placeholder="123456" />
+                       
+                    </div>
+                    <div className="pt-2">
+                        <button 
+                        onClick={()=>{
+                            axios.post('http://localhost:3000/api/user', {
+                                username,
+                                password
+                            })
+                        }}
+                        type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
             </a>
@@ -29,11 +45,11 @@ interface LabelledInputType {
     label: string;
     placeholder: string;
     type?: string;
+    onChange: ChangeEventHandler<HTMLInputElement>
 }
-
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
+function LabelledInput({ label, placeholder, type, onChange }: LabelledInputType) {
     return <div>
         <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-        <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
     </div>
 }
